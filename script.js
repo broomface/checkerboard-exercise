ae92ae0e4b25532eea3a564531ae80829a9335e5
nrfi@sbcglobal.net <scott.m.rushford@gmail.com>
2017-10-19 13:34:19 -0600

    updated/added files for project

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-// Your JS goes here
+for (let i = 0; i < 81; i++){
+  var body = document.getElementsByClassName("grid")[0];
+  var div = document.createElement("div");
+  document.body.appendChild(div);
+  div.style.width = "11.1%"
+  div.style.paddingBottom = "11.1%"
+  div.display = "float";
+  div.style.float = "left";
+
+  if (i % 2 === 0){
+    div.style.backgroundColor = "tan";
+  }
+  if (i % 2 === 1){
+  div.style.backgroundColor = "black";
+
+}
+}
